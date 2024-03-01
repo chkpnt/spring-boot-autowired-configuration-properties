ccb6827beccf511ff2ab1110ab9a19b664655779
Gregor Dschung <gregor@chkpnt.de>
2024-03-01 15:03:09 +0100

    Upgrade to Spring Boot 3.2.3

--- src/main/java/com/example/demo/configurationproperties/AuthPropertiesImpl.java
+++ src/main/java/com/example/demo/configurationproperties/AuthPropertiesImpl.java
@@ -1,6 +1,6 @@
 package com.example.demo.configurationproperties;
 
-import javax.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotEmpty;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.validation.annotation.Validated;
